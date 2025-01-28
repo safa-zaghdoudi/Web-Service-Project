8f3275c17a8f2e6f632983ad1f18d5199e903498
safa-zaghdoudi <157115385+safa-zaghdoudi@users.noreply.github.com>
2025-01-29 00:29:30 +0100

    Delete backend directory

--- backend/ressources/__init__.py
+++
@@ -1,7 +0,0 @@
-# from .from .residency import residency_bp
-
-#residency import residency_bp
-
-# # Blueprint registry
-# def register_blueprints(app):
-#     app.register_blueprint(residency_bp, url_prefix="/api")
